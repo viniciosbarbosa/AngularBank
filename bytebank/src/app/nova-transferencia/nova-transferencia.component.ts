4b54ee4718f5e79cf6ce4895ca822d76d619743d
vinicios.barbosa <viniciosbarbosagit@gmail.com>
2022-01-15 20:06:23 -0300

    first commit | comecando com o angular

---
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,27 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+
+
+  //any deve estar colada ao event retorna um objeto dentro de outro -|-
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0
+  }
+}
